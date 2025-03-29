418d180b03d82d5275ce4c54ef8b5ec575353a32
Jagannnnnn <jaganmohan85220@gmail.com>
2025-03-29 11:33:19 +0530

    learn ansible

--- intro/main.tf
+++ intro/main.tf
@@ -8,6 +8,14 @@
     default = 10
   }
 
+  # List Variable
+
+  variable "y" {
+    default = [ 1, "abc", 0,3,false]
+  }
+# list can have a combination of types also
+
+
   # Print variable on output
   output "x" {
     value = var.x
@@ -16,4 +24,8 @@
   # if our value is just variable then var.x,if it is combination of strings then "${var.x}"
   output "x1" {
     value = "value of x - ${var.x}"
+  }
+
+  output "y1" {
+    value = var.y[0]
   }
