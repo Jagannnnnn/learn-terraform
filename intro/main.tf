c85e9251edeeadd75179b4c2ae8632daac3c1df6
Jagannnnnn <jaganmohan85220@gmail.com>
2025-03-29 11:21:18 +0530

    learn ansible

--- intro/main.tf
+++ intro/main.tf
@@ -3,3 +3,17 @@
   content = "Hello World"
   filename = "/tmp/foo.bar"
 }
+  # Normal Variable
+  variable "x" {
+    default = 10
+  }
+
+  # Print variable on output
+  output "x" {
+    value = var.x
+  }
+
+  # if our value is just variable then var.x,if it is combination of strings then "${var.x}"
+  output "x1" {
+    value = "value of x - ${var.x}"
+  }
