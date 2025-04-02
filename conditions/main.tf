3d7f4b7b4ce1be98559a74b4b34ff73ae2e6f647
Jagannnnnn <jaganmohan85220@gmail.com>
2025-04-02 19:54:34 +0530

    learn ansible

--- conditions/main.tf
+++ conditions/main.tf
@@ -11,3 +11,12 @@ variable "content" {
 resource "null_resource" "test" {
   count = var.content == null ? 0 : 1
 }
+
+
+variable "x" {
+  type = number
+}
+output "x" {
+  value = var.x == 1 ? 100 : (var.x == 2 ? 200 : 0)
+
+}
